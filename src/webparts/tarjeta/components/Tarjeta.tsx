1a11a2138871ded6fc3eaf01a8c7f073a07331d8
Ikigai <dave.corredor@gmail.com>
2023-11-19 21:34:47 -0500

    Incorporacion inicila de estilos y propiedades

--- src/webparts/tarjeta/components/Tarjeta.tsx
+++ src/webparts/tarjeta/components/Tarjeta.tsx
@@ -6,20 +6,17 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class Tarjeta extends React.Component<ITarjetaProps, {}> {
   public render(): React.ReactElement<ITarjetaProps> {
     return (
-      <div className={ styles.tarjeta }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.titulo)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
+      <div className={styles.container}>
+        <div className={styles.sectionImg}>
+              <img src={this.props.urlImagen} alt="Imagen" />
+            </div>
+            <div className={styles.sectionText}>
+              <div className={styles.sectionDescription}>
+                <h1>{this.props.titulo}</h1>
+                <p>{this.props.texto}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
     );
   }
 }
